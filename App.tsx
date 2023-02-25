894093d426b065437280a2fc1bb09b6280c003aa
ghazali-web <dejahichem96@gmail.com>
2023-02-26 00:40:37 +0100

    changed

--- App.tsx
+++ App.tsx
@@ -8,17 +8,13 @@
 import React from 'react';
 
 import {SafeAreaView, StyleSheet} from 'react-native';
+import Navigation from './src/navigation/index.js';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-import ConfirmEmailScreen from './src/screnns/confirmEmailScreen/ConfirmEmailScreen.js';
-import ForgotPasswordScreen from './src/screnns/ForgotPasswordScreen/ForgotPasswordScreen.js';
-import NewPasswordScreen from './src/screnns/NewPasswordScreen/NewPasswordScreen.js';
-import SigninScreen from './src/screnns/SigninScreen/Signin_Screen.js';
-import {SignUpScreen} from './src/screnns/SignUpScreen/index.js';
 
 function App(): JSX.Element {
   return (
     <SafeAreaView style={styles.root}>
-      <NewPasswordScreen />
+      <Navigation />
     </SafeAreaView>
   );
 }
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,11 +1,24 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import ConfirmEmailScreen from '../screnns/confirmEmailScreen/ConfirmEmailScreen.js';
+import ForgotPasswordScreen from '../screnns/ForgotPasswordScreen/ForgotPasswordScreen.js';
+import NewPasswordScreen from '../screnns/NewPasswordScreen/NewPasswordScreen.js';
+import SigninScreen from '../screnns/SigninScreen/Signin_Screen.js';
+import {SignUpScreen} from '../screnns/SignUpScreen/index.js';
 const Navigation = () => {
+  const Stack = createNativeStackNavigator();
   return (
-    <View>
-      <Text>Navigation</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerٍShown: false}}>
+        <Stack.Screen name="Signin" component={SigninScreen} />
+        <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+        <Stack.Screen name="NewPassword" component={NewPasswordScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
